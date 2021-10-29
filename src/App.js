724c5bf22c29fa09ea51ef6b2b7a458005deee92
Engel Reyes <engelreyes23@gmail.com>
2021-10-29 16:26:19 -0600

    Add useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./components/Header";
+import Formulario from "./components/Formulario";
 
 function App() {
+	// Define State
+	const [cantidad, setCantidad] = useState(0);
+
 	return (
 		<Fragment>
-			<Header />
+			<Header
+				titulo="Cotizador de prestamos"
+				descripcion="Utiliza el formulario y obten una cotizacion"
+			/>
+			<div className="container">
+				<Formulario cantidad={cantidad} setCantidad={setCantidad} />
+			</div>
 		</Fragment>
 	);
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Header() {
-	return <h1>Hola Mundo desde components</h1>;
-}
+const Header = ({ titulo }) => {
+	return <h1>{titulo}</h1>;
+};
 
 export default Header;
