a376b1f8e7a305ccf2cc87041c5292ce7278cd6e
Engel Reyes <engelreyes23@gmail.com>
2021-10-29 18:34:52 -0600

    fin

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -3,10 +3,18 @@ import React from "react";
 const resultado = ({ cantidad, plazo, total }) => (
 	<div className="u-full-width resultado">
 		<h2>Resumen</h2>
-		<p>Cantidad solicitada: ${cantidad}</p>
-		<p>Plazo: {plazo} meses</p>
-		<p>Pagos Mensuales de: ${(total / plazo).toFixed(2)}</p>
-		<p>Total a pagar: ${total.toFixed(2)}</p>
+		<p>
+			<strong>Cantidad solicitada:</strong> ${cantidad}
+		</p>
+		<p>
+			<strong>Plazo:</strong> {plazo} meses
+		</p>
+		<p>
+			<strong>Pagos Mensuales de:</strong> ${(total / plazo).toFixed(2)}
+		</p>
+		<p>
+			<strong>Total a pagar:</strong> ${total.toFixed(2)}
+		</p>
 	</div>
 );
 
